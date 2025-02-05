1e9449277afa7a948744e09c19b09554b1daf37a
dorothee <keza@gmail.com>
2025-02-05 18:53:22 +0200

    Update project files

--- bs/src/components/Navigation.js
+++ bs/src/components/Navigation.js
@@ -8,6 +8,18 @@ const Navigation = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/location">Location</Link></li> {/* Added link to Location page */}
+                <span style={{ 
+                    color: 'transparent', 
+                    fontSize: '24px', 
+                    textShadow: '2px 2px 4px white', 
+                    background: 'rgba(76, 175, 80, 0.7)', // Updated background with opacity
+                    WebkitBackgroundClip: 'text', 
+                    display: 'inline-block', 
+                    float: 'left', 
+                    margin: '0', 
+                    padding: '0' 
+                }}>bs_ms</span>
+
                 <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
