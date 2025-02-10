4f2a361049de2b99b663fb3d71d30555c988f32d
dorothee <keza@gmail.com>
2025-02-10 11:50:03 +0200

    Removed HomePage and updated routing

--- bs/src/App.js
+++ bs/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
 import LocationPage from './components/LocationPage';
@@ -11,7 +10,6 @@ const App = () => {
         <Router>
             <Navigation />
             <Routes>
-                <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/contact" element={<ContactPage />} />
                 <Route path="/location" element={<LocationPage />} />
